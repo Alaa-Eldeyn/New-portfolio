b8edf198fe46c93e4aa09906de008aca0d699373
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2025-02-09 12:53:55 +0200

    Add Vercel Analytics integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ScrollUp from "@/components/ScrollUp";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
 
 const cairo = localFont({
   src: "./fonts/Cairo-Bold.woff2",
@@ -32,6 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
+      <Analytics />
       <body className={` ${cairo.variable} antialiased`}>
         <ScrollUp />
         {children}
